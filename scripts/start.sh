be490373b167941fe748231d00caa0f6ffd429d2
Torge Matthies <openglfreak@googlemail.com>
2020-05-17 18:49:38 +0200

    Add license header to scripts.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+# Copyright (C) 2020 Torge Matthies
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Author contact info:
+#   E-Mail address: openglfreak@googlemail.com
+#   PGP key fingerprint: 0535 3830 2F11 C888 9032 FAD2 7C95 CD70 C9E8 438D
 
 # https://stackoverflow.com/a/29835459
 : "${script_dir:="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)"}"
